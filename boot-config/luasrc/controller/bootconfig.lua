5591774de752075852b936a80560617c9bee0a3f
melsem <36903427+melsem@users.noreply.github.com>
2021-12-04 17:27:31 +0200

    Update bootconfig.lua
    The bootconfig editor was displayed on the main menu before entering the password.

--- boot-config/luasrc/controller/bootconfig.lua
+++ boot-config/luasrc/controller/bootconfig.lua
@@ -2,6 +2,6 @@ module("luci.controller.bootconfig", package.seeall)
 
 function index()
 
-   entry({"admin", "system", "bootconfig"}, template("boot-config/bootconfig"), _("Boot-config"),88).dependent = true
+   entry({"admin", "system", "bootconfig"}, template("boot-config/bootconfig"), _("Boot-config"),88).acl_depends={ "unauthenticated" }
 
 end
